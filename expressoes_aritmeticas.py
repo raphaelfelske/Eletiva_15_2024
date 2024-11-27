225b700aa2c67af341fadcf7434a7044a3b8974b
raphaelfelske <raphaelsf@edu.colegioanchieta.g12.br>
2024-11-27 20:54:26 -0300

    expressoes_aritmeticas.py

---
+++ expressoes_aritmeticas.py
@@ -0,0 +1,13 @@
+resultado = 2*6/3
+# Calcula a expressão
+ 
+print("O resultado da expressão 2*6/3 é:", resultado)
+#Exibe o resultado da expressão
+ 
+resultado = 3+2*4
+ 
+print("O resultado da expressão 3+2*4 é:", resultado)
+ 
+resultado = 2*3**4
+ 
+print("O resultado da expressão 2*3**4 é:", resultado)
