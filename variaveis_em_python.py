10107b11dad563285d0a12dd0411ca60382085e1
raphaelfelske <raphaelsf@edu.colegioanchieta.g12.br>
2024-11-27 20:55:41 -0300

    variaveis_em_python.py

---
+++ variaveis_em_python.py
@@ -0,0 +1,9 @@
+# variaveis_tipos
+ 
+# Atribuindo valores a variáveis
+cidade = "Porto Alegre"
+populacao = 1484941
+ 
+# Usando variáveis em operações
+populacao_ano_que_vem = populacao + 10000
+print(f"A população de {cidade} no próximo ano será de aproximadamente {populacao_ano_que_vem} pessoas.")
