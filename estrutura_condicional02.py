7c8bcefb4cbb851b60eca5076060d19e158a4f13
raphaelfelske <raphaelsf@edu.colegioanchieta.g12.br>
2024-11-27 22:06:58 -0300

    Create estrutura_condicional02.py

---
+++ estrutura_condicional02.py
@@ -0,0 +1,12 @@
+nota: int
+
+nota= int(input("Digite a sua nota:
+
+if nota >= 90:
+print("A nota é A.")
+elif nota >= 80:
+print("A nota é B.")
+elif nota >= 70:
+print("A nota é c.")
+else:
+print("A nota é D ou F.")
