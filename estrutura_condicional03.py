8bd0a08c22b6a9faa0f627072f2dccc008153ce7
raphaelfelske <raphaelsf@edu.colegioanchieta.g12.br>
2024-11-27 22:23:10 -0300

    Create estrutura_condicional03.py

---
+++ estrutura_condicional03.py
@@ -0,0 +1,17 @@
+# Solicita ao usuário que digite o número inteiro
+
+from unicodedata import numeric
+
+numero
+
+- int(input("Digite um número inteiro:
+
+# Verifica se o número é positivo, negativo ou zero
+
+if numero > 0:
+print(f"O número {numero} é positivo.")
+
+if
+
+numero < 0:
+print(f"O número {numero} é negativo.")
